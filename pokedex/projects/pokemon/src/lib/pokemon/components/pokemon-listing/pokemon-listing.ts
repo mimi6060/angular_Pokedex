80a878409b552300bce9cfdfde5f152899504da8
mimi6060 <mm6060.michel@hotmail.com>
2025-07-29 19:20:43 +0200

    http call

--- pokedex/projects/pokemon/src/lib/pokemon/components/pokemon-listing/pokemon-listing.ts
+++ pokedex/projects/pokemon/src/lib/pokemon/components/pokemon-listing/pokemon-listing.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Pokemon } from '../../services/pokemon';
+import { RouterModule } from '@angular/router';
+import { CommonModule, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'lib-pokemon-listing',
-  imports: [],
+  imports: [ CommonModule, RouterModule , NgIf, NgForOf],
   templateUrl: './pokemon-listing.html',
   styleUrl: './pokemon-listing.css'
 })
 export class PokemonListing {
+    service = inject(Pokemon);
 
+  next() {
+    const offset = this.service.offset() + this.service.limit;
+    this.service.setOffset(offset);
+  }
+
+  prev() {
+    const offset = this.service.offset() - this.service.limit;
+    this.service.setOffset(Math.max(0, offset));
+  }
 }
--- pokedex/projects/pokemon/src/lib/pokemon/services/pokemon.ts
+++ pokedex/projects/pokemon/src/lib/pokemon/services/pokemon.ts
@@ -1,8 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Pokemon {
-  
+  private http = inject(HttpClient);
+  private api = 'https://pokeapi.co/api/v2/pokemon';
+
+  // Signals
+  offset = signal(0);
+  limit = 20;
+
+  response = signal<any | null>(null);
+  loading = signal(false);
+
+  // Derived signal (computed)
+  pokemons = computed(() => this.response()?.results ?? []);
+  next = computed(() => this.response()?.next ?? null);
+  previous = computed(() => this.response()?.previous ?? null);
+
+  constructor() {
+    effect(() => {
+      this.fetchList();
+    });
+  }
+    setOffset(newOffset: number) {
+    this.offset.set(newOffset);
+  }
+
+  fetchList() {
+    this.loading.set(true);
+    this.http
+      .get(`${this.api}?limit=${this.limit}&offset=${this.offset()}`)
+      .subscribe((res) => {
+        this.response.set(res);
+        this.loading.set(false);
+      });
+  }
+
+  getPokemon(name: string) {
+    return this.http.get(`${this.api}/${name}`);
+  }
 }
